62c51a3eb47939aabbe3375a795a809bb063186a
Late Night <eqemmett@gmail.com>
2023-02-19 13:55:17 -0500

    list items

--- main.lua
+++ main.lua
@@ -1,2 +1,13 @@
-local barrels = peripheral.find("barrel")
-print(barrels)
+local barrels = {peripheral.find("minecraft:barrel")}
+local items = {}
+for index, barrel in pairs(barrels) do
+    for slot, item in pairs(barrels.list()) do
+        if not items[item.name] then
+            items[item.name] = 0
+        end
+        items[item.name] = items[item.name] + item.count
+    end
+end
+for index, titem in pairs(items) do
+    print(index,titem)
+end
