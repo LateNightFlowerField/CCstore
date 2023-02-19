ec50d3aa4765c920557bb26bbf28098de1b754fb
Late Night <eqemmett@gmail.com>
2023-02-19 13:58:44 -0500

    display names

--- main.lua
+++ main.lua
@@ -3,11 +3,11 @@ local items = {}
 for index, barrel in pairs(barrels) do
     for slot, item in pairs(barrel.list()) do
         if not items[item.name] then
-            items[item.name] = 0
+            items[item.name] = {0,barrel.getItemDetail(slot).displayName}
         end
-        items[item.name] = items[item.name] + item.count
+        items[item.name] = {items[item.name][1] + item.count, items[item.name][2]}
     end
 end
 for index, titem in pairs(items) do
-    print(index,titem)
+    print(titem[2],titem[1])
 end
